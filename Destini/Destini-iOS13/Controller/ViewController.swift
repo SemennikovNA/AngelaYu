c68136ea7decba8ffe0e51ccd072f4b3d6c896e3
Nikita <semennikovna@yandex.ru>
2023-11-07 23:13:20 +0600

    Create user interface

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IB Outlet's
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var choiceTwo: UIButton!
+    
+    //MARK: - Properties
+    
+    let story = StoryBrain()
+    let storyZeroandChoice = ["You see a fork in the road.", "Take a left", "Take a right"]
+    
+    //MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    
+    //MARK: - IB Action's
 
-
+    @IBAction func choiceButtonPressed(_ sender: UIButton) {
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyZeroandChoice[0]
+        
+    
+        
+        // Configure button
+        choiceOne.layer.cornerRadius = 20
+        choiceTwo.layer.cornerRadius = 20
+        choiceOne.setTitle(storyZeroandChoice[1], for: .normal)
+        choiceTwo.setTitle(storyZeroandChoice[2], for: .normal)
+    }
 }
-
