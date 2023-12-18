589e75d6fd54bfdca5953cadc5056f208f2db5ad
Nikita <semennikovna@yandex.ru>
2023-12-18 23:54:17 +0600

    Added firebase in project

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,22 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = " "
+        
        
         // Call function's
         setupView()
     }
     
     private func setupView() {
-        var charIndex: Double = 0
-        let textForTitle = "⚡️ Flash chat"
-        for letter in textForTitle {
-            Timer.scheduledTimer(withTimeInterval: 0.1 *  charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️ Flash chat"
     }
 }
